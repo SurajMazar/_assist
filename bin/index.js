8854fa01bf8ae45481d42d910704377082eb9e3f
Suraz Magar <suraj.thapa@manifera.com>
2022-08-07 11:06:46 +0545

    CLI

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+const app = require('../bootstrap/app')
+
+/**
+ * COMMAND RUNS HERE
+ */
+app()
--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./bootstrap/app')
-
-/**
- * COMMAND RUNS HERE
- */
-app()
